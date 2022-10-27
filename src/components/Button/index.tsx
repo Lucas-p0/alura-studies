a94c6529d09adb18fdec0a7548b1fbc4a927546f
Lucas pinheiro <lucaslogos@gmail.com>
2022-10-27 14:20:25 -0300

    UpDate

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,12 +1,15 @@
 import React from 'react';
+import '../Button/Button.css';
 
-class Botao extends React.Component {
-    render() {
-       return (
-           <button>
-             Botão
-           </button>
-          )
-       }
-    }
-  export default Botao; 
+function Botao () {
+    
+  return (
+    <button className="botao">
+      Botão
+    </button>
+  )
+}
+    
+export default Botao; 
+
+   
