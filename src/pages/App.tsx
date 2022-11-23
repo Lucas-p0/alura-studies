38ed04ae4ed849f285007924d290949832d12b1b
Lucas pinheiro <lucaslogos@gmail.com>
2022-11-23 13:15:29 -0300

    UpDate

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import './style.scss'
 import Lista from '../components/Lista';
 import Formulario from "../components/Formulario"
 
 function App() {
   return (
-    <div className="App">
+    <div className="AppStyle">
       <Formulario />
       <Lista/>
     </div>
