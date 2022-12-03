0deebdb48f040521b40f1f7c260b0bbe8491ad90
Lucas pinheiro <lucaslogos@gmail.com>
2022-12-03 14:00:09 -0300

    update

--- src/components/Formulario/index.tsx
+++ src/components/Formulario/index.tsx
@@ -1,36 +1,40 @@
 import React from "react";
 import Botao from "../Button"
-import '../Formulario/Formulario.css'
+import '../Formulario/Formulario.scss'
 
 class Formulario extends React.Component {
   render() {
     return (
-      <form className="formulario">        
-          <div>
-            <label htmlFor="tarefa">Adicione um novo estudo: </label>
-            <input
-              type="text"
-              name="tarefa"
-              id="tarefa"
-              placeholder="O que você deseja estudar?"
-              required
-            />
-          </div>
-          <div>
-            <label htmlFor="tempo">Tempo: </label>
-            <input
-              type="time"
-              step="1"
-              name="tempo"
-              id="tempo"
-              min="00:00:00"
-              max="01:30:00"
-              required
-            />
-          </div>        
-        <Botao/>
+      <form className="novaTarefa">
+        <div className="inputContainer">
+          <label htmlFor="tarefa">
+            Adicione um novo estudo
+          </label>
+          <input
+            type="text"
+            name="tarefa"
+            id="tarefa"
+            placeholder="O que você quer estudar"
+            required
+          />
+        </div>
+        <div className="inputContainer">
+          <label htmlFor="tempo">
+            Tempo
+          </label>
+          <input
+            type="time"
+            step="1"
+            name="tempo"
+            id="tempo"
+            min="00:00:00"
+            max="01:30:00"
+            required
+          />
+        </div>
+        <Botao />
       </form>
-    );
+    )
   }
 }
 
